18ff255a90847c7e4bbc72ad132299b8b597d00c
MarianoHMargarian <89165560+marianohm@users.noreply.github.com>
2026-05-09 12:33:12 -0300

    Mas Cambios

--- Reveire/assets/js/script.js
+++ Reveire/assets/js/script.js
@@ -41,3 +41,40 @@ function login() {
     })
     .catch(err => alert("Credenciales inválidas"));
 }
+
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+});
